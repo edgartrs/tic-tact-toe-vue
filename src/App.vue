b3270f51ced3cc1c1ab5a7abcb80409cf4940995
Edgar Torres <edgartrs@gmail.com>
2023-12-14 01:30:11 -0600

    feat: Add single player mode, ability to select game type

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import BoardSquare from "./components/BoardSquare.vue"
 type Player = {
     name: string
     symbol: string
+    isBot: boolean
 }
 
 type Square = {
@@ -33,11 +34,13 @@ const winSquares = ref<Array<Square>>([])
 const playerOne: Player = {
     name: "Player One",
     symbol: "X",
+    isBot: false,
 }
 
 const playerTwo: Player = {
     name: "Player Two",
     symbol: "O",
+    isBot: false,
 }
 
 const currentPlayer = ref(playerOne)
@@ -45,10 +48,30 @@ const winner = ref<Player | null>(null)
 const status = computed(() => {
     if (winner.value) {
         return `Winner: ${winner.value.name} (${winner.value.symbol})!!!`
+    } else if (currentPlayer.value.isBot) {
+        return `Turn: ${currentPlayer.value.name} (${currentPlayer.value.symbol}) is thinking...`
     } else {
         return `Turn: ${currentPlayer.value.name} (${currentPlayer.value.symbol})`
     }
 })
+
+const botTurn = () => {
+    if (winner.value) return
+
+    const emptySquares = board.value.filter((square) => square.symbol === "")
+    const randomSquare =
+        emptySquares[Math.floor(Math.random() * emptySquares.length)]
+
+    // wait 2 seconds before bot turn
+    setTimeout(() => {
+        turn(randomSquare, currentPlayer.value)
+    }, 1000)
+}
+
+const nextPlayer = () => {
+    currentPlayer.value =
+        currentPlayer.value.symbol === playerOne.symbol ? playerTwo : playerOne
+}
 
 const turn = (square: Square, player: Player) => {
     if (winner.value) return
@@ -64,8 +87,15 @@ const turn = (square: Square, player: Player) => {
         console.log(`Winner! ${currentPlayer.value.name}`)
         console.table(winSquares.value)
     }
-    currentPlayer.value =
-        player.symbol === playerOne.symbol ? playerTwo : playerOne
+    // currentPlayer.value =
+    //     player.symbol === playerOne.symbol ? playerTwo : playerOne
+
+    if (singlePlayer && !currentPlayer.value.isBot) {
+        nextPlayer()
+        botTurn()
+    } else {
+        nextPlayer()
+    }
 }
 
 const reset = () => {
@@ -75,6 +105,8 @@ const reset = () => {
     })
     winner.value = null
     currentPlayer.value = playerOne
+
+    openDialog()
 }
 
 const getWinSquares = () => {
@@ -143,6 +175,29 @@ const getWinSquares = () => {
 
     return winSquares
 }
+
+const dialog = ref<HTMLDialogElement>()
+const openDialog = () => {
+    dialog.value?.showModal()
+}
+const closeDialog = () => {
+    dialog.value?.close()
+}
+
+let singlePlayer = false
+
+const startSinglePlayerGame = () => {
+    reset()
+    closeDialog()
+    singlePlayer = true
+    playerTwo.isBot = true
+}
+
+const startTwoPlayerGame = () => {
+    reset()
+    closeDialog()
+    singlePlayer = false
+}
 </script>
 
 <template>
@@ -160,6 +215,24 @@ const getWinSquares = () => {
                 <h2 class="text-l mt-8">
                     {{ status }}
                 </h2>
+                <dialog
+                    ref="dialog"
+                    class="mt-8 rounded-xl bg-gray-500 p-6 shadow-3xl backdrop:bg-black/15 backdrop:backdrop-blur-md"
+                >
+                    <p class="mt-3 mb-3 text-slate-300">Choose game type:</p>
+                    <button
+                        class="bg-slate-300 text-gray-900 rounded-lg p-2 mr-5"
+                        @click="startSinglePlayerGame"
+                    >
+                        Single Player
+                    </button>
+                    <button
+                        class="bg-slate-300 text-gray-900 rounded-lg p-2"
+                        @click="startTwoPlayerGame"
+                    >
+                        Two Players
+                    </button>
+                </dialog>
                 <div class="grid grid-cols-3 gap-1 w-48">
                     <BoardSquare
                         v-for="square in board"
